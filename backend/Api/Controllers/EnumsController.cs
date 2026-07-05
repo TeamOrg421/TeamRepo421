05ad392c524ac36db6a63a6152333892b1fc83eb
Андрій Дячук <ad1379125@gmail.com>
2026-07-05 19:32:57 +0300

    Enum controller (#35)
    * .
    
    * .

---
+++ backend/Api/Controllers/EnumsController.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/enums")]
+    [ApiController]
+    public class EnumsController : ControllerBase 
+    {
+        // тут використовується -> Reflection (Рефлексія)
+        // Bid - для доступу в збірку, де знаходяться всі enum
+        [HttpGet("allNums")]
+        public async Task<ActionResult<List<string>>> GetAllNumsAsync()
+        {
+            var dataAccessAssembly = typeof(Bid).Assembly;
+
+            var enumNames = dataAccessAssembly.GetTypes()
+                .Where(t => t.IsEnum) 
+                .Select(t => t.Name)  
+                .ToList();
+
+            return Ok(enumNames);
+        }
+        [HttpGet("enumValues/{enumName}")]
+        public async Task<ActionResult<List<string>>> GetEnumValuesAsync(string enumName)
+        {
+            var dataAccessAssembly = typeof(Bid).Assembly;
+
+            var enumType = dataAccessAssembly.GetTypes()
+                .FirstOrDefault(t => t.IsEnum && t.Name.Equals(enumName, StringComparison.OrdinalIgnoreCase));
+
+            if (enumType == null)
+            {
+                return NotFound($"Enum '{enumName}' not found.");
+            }
+
+            var enumValues = Enum.GetNames(enumType).ToList();
+
+            return Ok(enumValues);
+        }
+    }
+}
