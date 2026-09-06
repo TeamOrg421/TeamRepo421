5084f7633835c136f572a3843d15c0c79de39515
epimix <epimiks@gmail.com>
2026-09-06 10:55:31 +0300

    Watchlist (#106)
    * created working watchlist
    
    * watchlsit update
    
    ---------
    
    Co-authored-by: Kelwoi <vova0110gordienko@gmail.com>
    Co-authored-by: Андрій Дячук <ad1379125@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-﻿import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
